6b43d404e7a40a078d15d8ca430b869ebc862edc
santhoshsridhar95 <santhoshsridhar95@gmail.com>
2021-09-10 01:35:19 +0530

    2nd commit

--- Java 8/src/com/san/java/StreamStartWith1.java
+++ Java 8/src/com/san/java/StreamStartWith1.java
@@ -1,10 +1,18 @@
 package com.san.java;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class StreamStartWith1 {
 
 	public static void main(String[] args) {
 		
 		List<Integer> myList = Arrays.asList(12,34,1,45,120,10);
+		
+		myList.stream()
+		.map(x->x+"")
+		.filter(x-> x.startsWith("1"))
+		.forEach(System.out::println);
 				
 				
 	}
