5fd3eeb01d0277f00be37ed244c958ffb00c9456
santhoshsridhar95 <santhoshsridhar95@gmail.com>
2021-09-10 01:30:17 +0530

    Stream Coding

---
+++ Java 8/src/com/san/java/StreamStartWith1.java
@@ -0,0 +1,12 @@
+package com.san.java;
+
+public class StreamStartWith1 {
+
+	public static void main(String[] args) {
+		
+		List<Integer> myList = Arrays.asList(12,34,1,45,120,10);
+				
+				
+	}
+
+}
